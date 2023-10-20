9c3e8df36a91b22c9abcaf51d93b5fe5f8deaffa
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2023-10-20 16:59:55 +0200

    example: boxed step

---
+++ tests/boxed-step.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect, Page } from '@playwright/test';
+
+// run these tests to see how error messages are displayed hiding implementation details of the boxed step. 
+// lines 29 and 39 have been commented out to show a failing test
+
+async function addToCart(page: Page){
+  await test.step('add to cart', async () => {
+    await page.getByRole('button', { name: 'Add To Bag' }).click();
+  }, { box: true });
+}
+
+test.describe('add to cart scenarios', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://cloudtesting.contosotraders.com/')
+  });
+
+  test('add to cart from carousel', async ({ page }) => {
+    await page.getByRole('button', { name: 'Buy Now' }).click();
+    await addToCart(page);
+    await page.getByLabel('cart').click();
+    await expect(page.getByText('Xbox Wireless Controller Lunar Shift Special Edition')).toBeVisible();
+  });
+
+  test('add to cart from search', async ({ page }) => {
+    const product = 'Xbox Wireless Controller Mineral Camo Special Edition'
+    const placeholder = page.getByPlaceholder('Search by product name or search by image')
+    await placeholder.click();
+    await placeholder.fill('xbox');
+    await placeholder.press('Enter');
+    // await page.getByRole('img', { name: product }).click();
+    await addToCart(page);
+    await page.getByLabel('cart').click();
+    await expect(page.getByText(product)).toBeVisible();
+  });
+
+  test('add to cart from all products page', async ({ page }) => {
+    const product = 'Xbox Wireless Controller Lunar Shift Special Edition'
+    await page.getByRole('link', { name: 'All Products' }).first().click();
+    // await page.getByRole('img', { name: product }).click();
+    await addToCart(page);
+    await page.getByLabel('cart').click();
+    await expect(page.getByText(product)).toBeVisible();
+  });
+
+  test('add to cart from laptops page', async ({ page }) => {
+    const product = 'Microsoft Surface Pro X 1876 13 Inches Laptop'
+    await page.getByRole('link', { name: 'Laptops' }).first().click();
+    await page.getByRole('img', { name:  product}).click();
+    await addToCart(page);
+    await page.getByLabel('cart').click();
+    await expect(page.getByText(product)).toBeVisible();
+  });
+
+});
